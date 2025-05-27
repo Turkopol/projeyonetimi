36d4e8bd4b22e0c638b8afe9c4b2a0860e32249d
Turkopol <gklkn@hotmail.com>
2025-05-27 15:14:44 +0300

    Add files via upload

---
+++ butce_modulu.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def run():
+    st.title("💰 Kaynak ve Bütçe Planlaması")
+    st.markdown("""
+    Bu bölümde proje kaynaklarının planlanması ve bütçenin oluşturulması süreçleri ele alınır.
+
+    - İnsan, ekipman, malzeme kaynakları
+    - Doğrudan ve dolaylı maliyetler
+    - Sabit ve değişken giderler
+    - Acil durum fonları
+    - Maliyet tahmin yöntemleri (Top-down, Bottom-up, Analog, Parametrik)
+
+    Örnek: Mekan kiralama bedeli 5.000 TL, davetiye 2.000 TL olarak tahmin edilir.
+    """)
